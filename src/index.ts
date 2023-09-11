4dd1385f24c33dfd2bee505b88ec3c364964af95
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-11 12:06:10 -0300

    add some actions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 #! /usr/bin/env node
 
-const hello = () => {}
+import { Command } from 'commander';
 
-console.log("wow")
+const program = new Command();
+
+program
+  .argument('<name>', 'Name of the user')
+  .option('-c, --capitalize', 'Capitalize name')
+  .action((name: string) => {
+    if (program.opts().capitalize) {
+      console.log(name.toUpperCase());
+    }
+    console.log(name);
+  })
+  .description("Say 'Hello'");
+
+program
+  .command('add <numbers...>')
+  .action((numbers: number[]) => {
+    const sum = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+    console.log(sum);
+  })
+  .description('Add numbers');
+
+program.parse(process.argv);
